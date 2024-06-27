842a96b6bd42cc1e49f10ccd98f7865cdf8736b5
7859897243 <sosadhruv740@gmail.com>
2024-06-27 18:28:06 +0530

    add

---
+++ src/App.jsx
@@ -0,0 +1,60 @@
+import { useState } from 'react';
+import './App.css';
+import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
+import Alert from './components/Alert';
+import Privacypolicy from './components/privacypolicy.jsx';
+
+
+function App() {
+
+
+  const [mode, setMode] = useState('light'); 
+
+  const [btnText, newbtnText] = useState('Enable Dark Mode');
+
+  const [alert, setAlert] = useState (null);
+
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type
+    });
+
+    setTimeout(() => {
+      setAlert(null);
+    }, 1500);
+  }
+
+  const toggleMode = () => {
+    if (mode === 'light') {
+      setMode('dark')
+      document.body.style.background = 'black';
+      document.body.style.color= 'white';
+      newbtnText('Enable to Dark Mode');
+      showAlert("Dark Mode Has Been Enable", "success")
+      setInterval(() => {
+        document.title = 'SIT - Admisson '
+      }, 2000);
+      setInterval(() => {
+        document.title = 'SIT - Bsc    it'
+      }, 1500);
+    } else {
+      setMode('light')
+      document.body.style.background = 'white';
+      document.body.style.color = 'black';
+      newbtnText('Enable to Light Mode');
+      showAlert("Light Mode Has Been Enable", "success")
+    }
+  }
+  return (
+    <>
+      <Navbar title="SIT" aboutText="Prince" mode={mode} toggleMode={toggleMode} btntext={btnText}></Navbar>
+      <Alert alert={alert}/> 
+      <TextForm heading="Intexr Text to Analyse Below" mode={mode} toggleMode={toggleMode} showAlert={showAlert}></TextForm>
+      <Privacypolicy />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components copy/privacypolicy.jsx
@@ -0,0 +1,66 @@
+import React from 'react'
+
+export default function privacypolicy() {
+  return (
+    <div>
+
+      <h2 className='mx-5 my-5  ' my-5>Privacy Policy</h2>
+<div className="accordion my-5 mx-5"  id="accordionExample" >
+  <div className="accordion-item">
+    <h2 className="accordion-header">
+      <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+        Privacy Policy
+      </button>
+    </h2>
+    <div id="collapseOne" className="accordion-collapse collapse show" data-bs-parent="#accordionExample">
+      <div className="accordion-body">
+      Welcome to Privacy Policy Online. Here you can generate your own Privacy Policy to comply with international laws on user data privacy (CCPA, CalOPPA, GDPR) and the requirements of leading Advertising and Affiliate Networks, such as Google AdSense or Commission Junction.
+
+On our site you will find not only a generator for quickly and easily generating a Privacy Policy for your sites, but also find many other resources for complying with regulations and learning more about Privacy Policies. What Is A Privacy Policy?
+A Privacy Policy is a legal document that discloses some or all of the ways a website gathers, uses, shares, discloses and/or manages a website visitor's data.
+
+Learn More
+      </div>
+    </div>
+  </div>
+  <div className="accordion-item">
+    <h2 className="accordion-header">
+      <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+        According to our company
+      </button>
+    </h2>
+    <div id="collapseTwo" className="accordion-collapse collapse" data-bs-parent="#accordionExample">
+      <div className="accordion-body">
+      Sample Privacy Policy Template
+We have gotten many requests for a standard Privacy Policy Template, so here it is! Simply replace the bracketed fields with your own information. Voilà!
+
+Learn More
+
+What Are Cookies
+What Are Cookies?
+Also known as browser cookies or tracking cookies, cookies are small, usually encrypted text files, located in your browser's directory.
+
+Learn More
+No warranty is provided regarding the accuracy or adequacy of these generators for every situation. If you are not sure about any legal questions, or have any questions or doubts about a Privacy Policy, a Terms & Conditions or Terms of Service or Terms of Use contract or a Disclaimer or a Disclosure, you should consult with a lawyer or law firm specialized in digital communications law. These generators are provided to our site visitors as a free service and is intended to operate as a "guideline". No warranty or guarantee is given nor implied about its use as a comprehensive or even partial solution to legal and/or regulatory obligations and/or best practices.If your business collects or uses personal information, you will be legally required to have and post a Privacy Policy.
+
+In this article, we will discuss the elements of a Privacy Policy to help you better understand the constructs of an effective Privacy Policy agreement that instills faith and trust in your customers and protects you from a number of liability issues.
+      </div>
+    </div>
+  </div>
+  <div className="accordion-item">
+    <h2 className="accordion-header">
+      <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+        According to government 
+      </button>
+    </h2>
+    <div id="collapseThree" className="accordion-collapse collapse" data-bs-parent="#accordionExample">
+      <div className="accordion-body">
+        <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+      </div>
+    </div>
+  </div>
+</div>
+
+    </div>
+  )
+}
